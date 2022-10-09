00df942ee8174c1ecfdd82feb0f766b4bd09bb2b
TheGiantNinja <yasserfedsi34@gmail.com>
2022-10-09 14:46:49 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const { DiscordAPIError } = require('discord.js')
+const Disord = require('discord.js')
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.console.log('Tyro is online');
+})
+
+
+
+
+client.login('MTAyODY0NTQ3MzAyOTk5NjY0NA.Gx9PBE.XzUq6xUXGLypzcx8IHUD9vIyMzDz8ak0r8jicQ');
