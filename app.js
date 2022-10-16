a2f8bc73cdc996625190c292c7a2da1ec761b925
TheGiantNinja <yasserfedsi34@gmail.com>
2022-10-16 20:22:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        message.channel.send('Pong')
+    }
+})
 
 client.login('MTAyODY0NTQ3MzAyOTk5NjY0NA.GMJC3U.eLdza6UPk5KS4ug_kDlRqvrxCA3lGO5a-P-_Kw');
