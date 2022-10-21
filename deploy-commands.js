15c5591ee3e416d0dbffd7c2448239ddfb90beab
TheGiantNinja <yasserfedsi34@gmail.com>
2022-10-21 22:22:59 +0200

    Added 3 new commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,12 +2,14 @@ const { REST, SlashCommandBuilder, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
+	new SlashCommandBuilder().setName('help').setDescription('Replies with help'),
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
     new SlashCommandBuilder().setName('developers').setDescription('Replies with autosoftPR'),
 	new SlashCommandBuilder().setName('version').setDescription('Replies with bot version'),
-	new SlashCommandBuilder().setName('button').setDescription('Replies with button')
+	new SlashCommandBuilder().setName('button').setDescription('Replies with button'),
+	new SlashCommandBuilder().setName('developer-ws').setDescription('Replies with developer wesbite'),
 ]
 	.map(command => command.toJSON());
 
